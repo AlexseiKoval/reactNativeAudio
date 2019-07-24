79b5b37bf18fd5ed8e10d45713ff9782e3987aa2
unknown <alexsei.koval@gmail.com>
2019-07-24 12:07:31 +0300

    Add existing file

--- App.js
+++ App.js
@@ -1,65 +1,96 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
+
  * @format
  * @flow
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Slider } from 'react-native';
+import { Text, View, Button, Slider, Alert, ScrollView, StyleSheet } from 'react-native';
 import Sound from 'react-native-sound';
-import { isTSTypeAliasDeclaration } from '@babel/types';
 
-// import MainView from './main.js';
-var sound = new Sound(undefined, () => { });
-
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-const audioRecord = [
-
-  {
-    id: 1,
-    title: 'mp3 #1',
-    url: require('./advertising.mp3'),
-    currentTime: 0,
-    lastTime:27,
-
-  },
-
-  {
-    id: 2,
-    title: 'aac #2',
-    url: require('./example.aac'),
-    currentTime: 0,
-    lastTime:36,
-  },
-  {
-    id: 3,
-    title: 'aac #3',
-    url: require('./example.aac'),
-    currentTime: 0,
-    lastTime:36,
-  },
-
-];
+var sound = new Sound(undefined, () => { }); // e
 
 type Props = {};
 export default class App extends Component<Props> {
 
   constructor(Props) {
     super(Props);
-    this.state = { curentRecord: { id: 0 }, ispause:false }
+    this.state = { curentRecord: { id: 0 }, ispause: false, audioRecord: [] }
   }
 
   componentDidMount() {
+    // Загрузка списка записей
+    // массив записей. 
+    // 
+    //------------
+    this.setState({
+      audioRecord: [
+        {
+          id: 1,
+          title: 'mp3 #1',
+          url: require('./audioRecord/advertising.mp3'),
+          currentTime: 0,
+          lastTime: 27,
+        },
+        {
+          id: 2,
+          title: 'aac #2',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+        {
+          id: 3,
+          title: 'aac #3',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+        {
+          id: 1,
+          title: 'mp3 #1',
+          url: require('./audioRecord/advertising.mp3'),
+          currentTime: 0,
+          lastTime: 27,
+        },
+        {
+          id: 2,
+          title: 'aac #2',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+        {
+          id: 3,
+          title: 'aac #3',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+        {
+          id: 1,
+          title: 'mp3 #1',
+          url: require('./audioRecord/advertising.mp3'),
+          currentTime: 0,
+          lastTime: 27,
+        },
+        {
+          id: 2,
+          title: 'aac #2',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+        {
+          id: 3,
+          title: 'aac #3',
+          url: require('./audioRecord/example.aac'),
+          currentTime: 0,
+          lastTime: 36,
+        },
+      ]
+    });
+    //-------------
 
   }
 
@@ -67,85 +98,99 @@ export default class App extends Component<Props> {
     clearInterval(this.timerId);
   }
 
-  mySetState = () => {
+
+  //*** функции  */
+  // установка интервала
+  setIntervalState = () => {
     sound.getCurrentTime((seconds) => {
-      audioRecord.map((el) => {
-        el.currentTime = (this.state.curentRecord.id == el.id) ? Math.round(seconds) : el.currentTime;
-        el.lastTime = (this.state.curentRecord.id == el.id) ? Math.round(sound.getDuration()) : el.lastTime;
-      
-      })
-
+      this.state.curentRecord.currentTime = Math.round(seconds);
+      // this.state.curentRecord.lastTime = Math.round(sound.getDuration()); //эту функции можно не использовать. использовать при загрузке файла.
       this.setState({});
-
     })
   }
+  //\установка интервала
 
-  timerId = undefined;
+  // функция нажатия кнопки.
+  onPressButton(curRecord = { id: 0 }) {
+    if ((this.state.curentRecord.id != curRecord.id) || ((this.state.curentRecord.id == curRecord.id) && (this.state.ispause))) {
 
+      this.setState({ curentRecord: curRecord, ispause: false });
+
+      clearInterval(this.timerId);//чистим интервал
+      sound.release(); //чистим
+
+      sound = new Sound(curRecord.url, (error) => {
+
+        if (error) { Alert.alert('ERROR. BAD FILE.'); return } // Проверка Файл мог не загрузится.
+
+        sound.setCurrentTime(curRecord.currentTime);
+        sound.stop(() => {
+          sound.play();
+        });
+
+        sound.setNumberOfLoops(-1); // по кругу.
+        this.timerId = setInterval(
+          this.setIntervalState, 1000);
+      });
+
+    } else {
+      this.setState({ ispause: true });
+      clearInterval(this.timerId);
+      sound.pause();
+    }
+  }
+
+  // функция бегунка
+  onChangeSlider = (value,curRecord) => {
+    if (curRecord.id == this.state.curentRecord.id) { sound.setCurrentTime(value * this.state.curentRecord.lastTime); }
+  }
+
+
+  //--------------------------------
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
+        <ScrollView contentContainerStyle={{ paddingVertical: 5 }}>
 
-        {audioRecord.map(curRecord => {
-          return (
-            <View style={{
+          {this.state.audioRecord.map(curRecord => {
+            return (
+              <View style={styles.audioRecordStyle}>
+                <View style={{ flex: 0.8, }} >
+                  <Text> {curRecord.title}   </Text>
 
-              flexDirection: 'row',
-              padding: 10,
-              alignSelf: 'stretch',
-              alignItems: 'center',
-              borderTopWidth: 1,
-              borderTopColor: 'rgb(180,180,180)',
-              borderBottomWidth: 1,
-              borderBottomColor: 'rgb(230,230,230)',
-            }}>
-              <View style={{ flex: 0.8, }} >
-              <Text> {curRecord.title}   </Text>
+                  <Slider
+                    value={((curRecord.lastTime) == 0 ? 0 : curRecord.currentTime / curRecord.lastTime)}
+                    onValueChange={(value) => { this.onChangeSlider(value, curRecord) }}
+                  />
+                  <Text> {curRecord.currentTime}  :  {curRecord.lastTime} </Text>
+                </View>
+                <View style={{ flex: 0.2, }} >
+                  <Button
+                    onPress={this.onPressButton.bind(this, curRecord)}
+                    title={((this.state.curentRecord.id == curRecord.id) && (!this.state.ispause)) ? 'pause' : 'play'} />
+                </View>
 
-                <Slider
-                  value={curRecord.currentTime/((curRecord.lastTime)==0?1:curRecord.lastTime)}
-                  onValueChange={value => {
-                    sound.setCurrentTime(value);
-                  } }
-                />
-                <Text> {curRecord.currentTime}  :  {curRecord.lastTime} </Text>
               </View>
-              <View style={{ flex: 0.2, }} >
-                <Button
-                  onPress={
-                    () => {
-                     
-                      if ((this.state.curentRecord.id != curRecord.id) || ((this.state.curentRecord.id == curRecord.id) && (this.state.ispause) )   ) {
-
-                        this.setState({ curentRecord: curRecord, ispause:false });
-
-                        clearInterval(this.timerId);
-                        sound.release();
-
-                        sound = new Sound(curRecord.url, (error) => {
-                          sound.setCurrentTime(curRecord.currentTime);
-                          sound.play();
-                          sound.setNumberOfLoops(-1);
-
-                          this.timerId = setInterval(
-                            this.mySetState, 1000);
-                        });
-
-                      } else {
-                        this.setState({ispause:true});
-                        clearInterval(this.timerId);
-                        sound.pause();
-                      }
-                    }
-                  }
-                  title={((this.state.curentRecord.id == curRecord.id)  && (!this.state.ispause)) ? 'pause' : 'play'} />
-              </View>
-
-            </View>
-          );
-        })}
-
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  audioRecordStyle: {
+
+    flexDirection: 'row',
+    padding: 10,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: 'rgb(180,180,180)',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgb(230,230,230)',
+  },
+
+});
